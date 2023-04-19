7b4bc17e7e1f0b480e279eb2221613c3744853b0
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2023-04-19 12:41:18 +0300

    lesson1, task2

--- src/main/java/lesson1/MainClass.java
+++ src/main/java/lesson1/MainClass.java
@@ -1,11 +1,24 @@
 package lesson1;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class MainClass {
     public static void main(String[] args) {
         //foo();
-        ex1();
+       // ex1();
+        int matrix2[][] = {{1,0,1},{1,1,0},{0,0,1}};
+        Random rnd = new Random();
+        int matrix[][] = new int[3][3];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = rnd.nextInt(2);
+            }
+        }
+        System.out.println(Arrays.deepToString(matrix));
+        System.out.println(ex2(matrix));
+        System.out.println(ex2(matrix2));
     }
     private static void foo() {
         try {
@@ -51,11 +64,11 @@ public class MainClass {
     private static int ex2(int[][] arr) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i].length != arr.length) {
+            if (arr[i].length != arr.length) { // проверяем длина строки под индексом i равна длине массива
                 throw new RuntimeException("Массив не квадратный!");
             }
             for (int j = 0; j < arr.length; j++) {
-                if (arr[i][j] != 0 && arr[i][j] != 1) {
+                if (arr[i][j] != 0 && arr[i][j] != 1) { // проверяем все ли элементы массива равны 0 или 1
                     throw new RuntimeException("В массиве есть элементы, отличные от 0 и 1!");
                 }
                 sum += arr[i][j];
